7614ca816c788d99004a42507d48929cea923533
Adib Mohsin <adibmohsin.root@gmail.com>
2022-08-02 21:33:21 +0600

    testing with demos

---
+++ demos.py
@@ -0,0 +1,37 @@
+demo1 = """The human coronavirus was first diagnosed in 1965 by Tyrrell and Bynoe from the respiratory tract sample of an adult with a common cold cultured on human embryonic trachea.1 Naming the virus is based on its crown-like appearance on its surface.2 Coronaviruses (CoVs) are a large family of viruses belonging to the Nidovirales order, which includes Coronaviridae, Arteriviridae, and Roniviridae families.3 Coronavirus contains an RNA genome and belongs to the Coronaviridae family.4 This virus is further subdivided into four groups, ie, the α, β, γ, and δ coronaviruses.5 α- and β-coronavirus can infect mammals, while γ- and δ- coronavirus tend to infect birds.6 Coronavirus in humans causes a range of disorders, from mild respiratory tract infections, such as the common cold to lethal infections, such as the severe acute respiratory syndrome (SARS), Middle East respiratory syndrome (MERS) and Coronavirus disease 2019 (COVID-19). The coronavirus first appeared in the form of severe acute respiratory syndrome coronavirus (SARS-CoV) in Guangdong province, China, in 20027 followed by Middle East respiratory syndrome coronavirus (MERS-CoV) isolated from the sputum of a 60-year-old man who presented symptoms of acute pneumonia and subsequent renal failure in Saudi Arabia in 2012.8 In December 2019, a β-coronavirus was discovered in Wuhan, China. The World Health Organization (WHO) has named the new disease as Coronavirus disease 2019 (COVID-19), and Coronavirus Study Group (CSG) of the International Committee has named it as SARS-CoV-2.9,10 Based on the results of sequencing and evolutionary analysis of the viral genome, bats appear to be responsible for transmitting the virus to humans"""
+
+demo2 = """Abstract. We address the question of which convex shapes, when
+packed as densely as possible under certain restrictions, fill the least
+space and leave the most empty space. In each different dimension and under each different set of restrictions, this question is
+expected to have a different answer or perhaps no answer at all.
+As the problem of identifying global minima in most cases appears
+to be beyond current reach, in this paper we focus on local minima. We review some known results and prove these new results:
+in two dimensions, the regular heptagon is a local minimum of
+the double-lattice packing density, and in three dimensions, the
+directional derivative (in the sense of Minkowski addition) of the
+double-lattice packing density at the point in the space of shapes
+corresponding to the ball is in every direction positive."""
+
+demo3 = """An n-dimensional convex body is a convex, compact, subset of R
+n
+with nonempty interior. The space of convex bodies, denoted Kn
+, can
+be endowed with the Hausdorff metric:
+dist(K, K0
+) = min{ε : K0 ⊆ Kε and K ⊆ K0
+ε},
+where Kε = {x + y : x ∈ K, ||y|| ≤ ε} is the ε-parallel body of K.
+A set of isometries Ξ is said to be admissible for K if the interiors
+of ξ(K) and ξ
+0
+(K) are disjoint for all distinct ξ, ξ0 ∈ Ξ. The (lower)
+mean volume of Ξ can be defined as d(Ξ) = lim infr→∞(4πr3/3)/|{ξ ∈
+Ξ : ||ξ(0)|| < r}|. The collection {ξ(K) : ξ ∈ Ξ} for an admissible Ξ is
+called a packing of K and said to be produced by Ξ. Its density is the
+fraction of space it fills: vol(K)/d(Ξ). The packing density of a body
+K, denoted δ(K) is the supremum of vol(K)/d(Ξ) over all admissible
+sets of isometries. Groemer proves some basic results about packing
+densities, including the fact that the supremum is actually achieved
+by some packing and the fact that δ(K) is continuous [10]. Groemer’s
+result apply also to the restricted packing densities which we define
+below."""
--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from text_sum import summarize
-
-demo = """The human coronavirus was first diagnosed in 1965 by Tyrrell and Bynoe from the respiratory tract sample of an adult with a common cold cultured on human embryonic trachea.1 Naming the virus is based on its crown-like appearance on its surface.2 Coronaviruses (CoVs) are a large family of viruses belonging to the Nidovirales order, which includes Coronaviridae, Arteriviridae, and Roniviridae families.3 Coronavirus contains an RNA genome and belongs to the Coronaviridae family.4 This virus is further subdivided into four groups, ie, the α, β, γ, and δ coronaviruses.5 α- and β-coronavirus can infect mammals, while γ- and δ- coronavirus tend to infect birds.6 Coronavirus in humans causes a range of disorders, from mild respiratory tract infections, such as the common cold to lethal infections, such as the severe acute respiratory syndrome (SARS), Middle East respiratory syndrome (MERS) and Coronavirus disease 2019 (COVID-19). The coronavirus first appeared in the form of severe acute respiratory syndrome coronavirus (SARS-CoV) in Guangdong province, China, in 20027 followed by Middle East respiratory syndrome coronavirus (MERS-CoV) isolated from the sputum of a 60-year-old man who presented symptoms of acute pneumonia and subsequent renal failure in Saudi Arabia in 2012.8 In December 2019, a β-coronavirus was discovered in Wuhan, China. The World Health Organization (WHO) has named the new disease as Coronavirus disease 2019 (COVID-19), and Coronavirus Study Group (CSG) of the International Committee has named it as SARS-CoV-2.9,10 Based on the results of sequencing and evolutionary analysis of the viral genome, bats appear to be responsible for transmitting the virus to humans"""
+from demos import demo3
 
 
 def main() -> None:
-    r = summarize(demo)
-    print(len(demo))
+    text = demo3
+    r = summarize(text)
+    print(len(text))
     print(len(r))
+    print(r)
 
 
 if __name__ == "__main__":
